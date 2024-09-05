1c8a0df7e431157ac33f08a01119ea147ed8ac72
Felix Gutierrez <fgutie35@stu.southtexascollege.edu>
2024-09-04 21:37:00 -0500

    Initial setup of working web page

---
+++ scripts/myScripts.js
@@ -0,0 +1,18 @@
+const newName = document.getElementById("firstName");
+const newNum = document.getElementById("phoneNum");
+const addButton = document.getElementById("contactBtn");
+const addedList = document.getElementById("addedNames");
+
+addButton.addEventListener("click", addItemFunc);
+function addItemFunc(){
+    if(newName.value != "") {
+        const addItem = document.createElement("li")
+        addItem.innerHTML = newName.value
+        addedList.appendChild(addItem)
+
+    }
+    else {
+        alert("Please put the name")
+    }
+
+}
